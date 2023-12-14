06f21bd907b205bd95a8881ad272b977ce69ff9c
Fernando Barreto Mendoza <fbarretomendoza@activecampaign.com>
2023-12-14 15:22:22 -0600

    yote

--- lua/fernando/remap.lua
+++ lua/fernando/remap.lua
@@ -1,3 +1,4 @@
+vim.keymap.set("n", "Q", "<nop>")
 -- keep search terms in the middle of screen
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
--- lua/fernando/set.lua
+++ lua/fernando/set.lua
@@ -4,7 +4,7 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 
 vim.opt.listchars = 'tab:→ ,trail:•'
 vim.opt.list = true

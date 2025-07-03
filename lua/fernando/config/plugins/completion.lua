34a28f9a41d37dfdcc011e956591091deb356f6b
Fernando Barreto Mendoza <fbarretomendoza@activecampaign.com>
2025-07-03 12:58:56 -0500

    add completion

---
+++ lua/fernando/config/plugins/completion.lua
@@ -0,0 +1,15 @@
+return {{
+  'saghen/blink.cmp',
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  version = '1.*',
+  opts = {
+    keymap = { preset = 'default' },
+
+    appearance = {
+      nerd_font_variant = 'mono'
+    },
+
+    completion = { documentation = { auto_show = false } },
+  },
+}}
--- lua/fernando/config/plugins/lsp.lua
+++ lua/fernando/config/plugins/lsp.lua
@@ -1,9 +1,13 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = { 'saghen/blink.cmp' },
   config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
       vim.lsp.enable('intelephense')
       vim.lsp.enable('jdtls')
       vim.lsp.enable('ts_ls')
+      vim.lsp.enable('lua_ls')
+      require('lspconfig').lua_ls.setup { capabilities = capabilities }
   end,
 }
 

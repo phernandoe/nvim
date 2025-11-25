4bd7a0284245e930bef9402c0b1a7799550b84e7
Fernando Barreto Mendoza <fbarretomendoza@activecampaign.com>
2025-11-25 15:57:47 -0600

    fix for nvim 0.11

--- lua/fernando/config/plugins/lsp.lua
+++ lua/fernando/config/plugins/lsp.lua
@@ -3,10 +3,11 @@ return {
     dependencies = { 'saghen/blink.cmp' },
     config = function()
         local capabilities = require('blink.cmp').get_lsp_capabilities()
+        vim.lsp.config('lua_ls', { setup = { capabilities = capabilities } })
+
         vim.lsp.enable('intelephense')
         vim.lsp.enable('jdtls')
         vim.lsp.enable('ts_ls')
         vim.lsp.enable('lua_ls')
-        require('lspconfig').lua_ls.setup { capabilities = capabilities }
     end,
 }
